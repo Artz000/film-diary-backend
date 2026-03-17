f91cd89086e316c534c37cd94280c5f59371eb2c
Zaporozhchenko <artemzaporozcenko@gmail.com>
2026-03-17 13:57:44 +0400

    fix: allow PATCH method in CORS

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'user-id']
 }));
 
